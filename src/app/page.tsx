347f48f8aa498ab6ad3caa996ae2f96b93403eb0
Facundo Carreño <facundo.carreno@bitlogic.io>
2024-04-03 16:59:08 -0300

    creacion de proyecto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-slate-700 text-xl'>hola mundo y Pili</h1>
+    </main>
+  );
+}
