b5f798edd3d120c70c917a77712c22eb24c780de
CarreFacu <facundo.carre@hotmail.com>
2024-04-06 16:22:28 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1 className='text-slate-700 text-xl'>hola mundo y Pili</h1>
     </main>
   );
